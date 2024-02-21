4ed7a977ef014d776222e706b17b0d34d48c2eae
luvvas <luvvasm@gmail.com>
2024-02-21 13:07:46 -0300

    feat: add dynamic product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,21 +9,44 @@ import { ColDef } from 'ag-grid-community';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ];
+  rowData: any = [];
+
+  gridOptions = {
+    rowHeight: 59
+  }
 
   // Column Definitions: Defines the columns to be displayed.
   colDefs: ColDef[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
+    { field: "p_name", headerName: "Product Name", sortable: true, headerClass: "header-cell" },
+    { field: "p_price", headerName: "Product Price", sortable: true, headerClass: "header-cell" },
+    { field: "p_description", headerName: "Product Description", sortable: true, headerClass: "header-cell" },
+    { field: "", headerName: "Actions", headerClass: "header-cell", width: 250, cellRenderer: this.actionRender }
   ];
 
-  constructor(private crudService: CRUDService) {
+  productList: any = [];
+  productListSubscribe: any;
 
+  constructor(private crudService: CRUDService) {
+  }
+
+  ngOnInit(): void {
+    this.getProductList()
+  }
+
+  getProductList() {
+    this.productListSubscribe = this.crudService.loadProducts().subscribe(res => {
+      this.productList = res;
+      this.rowData = res
+    })
+  }
+
+  actionRender(params: any) {
+    let div = document.createElement('div');
+    let htmlCode = '<button type="button" class="btn btn-success">View</button>\n' +
+    '<button type="button" class="btn btn-danger">Delete</button>\n' +
+    '<button type="button" class="btn btn-warning">Edit</button>'
+
+    div.innerHTML = htmlCode;
+    return div;
   }
 }
