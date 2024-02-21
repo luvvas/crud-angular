f9aaaaf14c37283db3cfd7590e8e81813128d405
luvvas <luvvasm@gmail.com>
2024-02-21 13:20:50 -0300

    feat: configure view button in productlist

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { CRUDService } from '../services/crud.service';
 
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ColDef } from 'ag-grid-community';
 
 @Component({
@@ -20,13 +21,13 @@ export class ProductListComponent {
     { field: "p_name", headerName: "Product Name", sortable: true, headerClass: "header-cell" },
     { field: "p_price", headerName: "Product Price", sortable: true, headerClass: "header-cell" },
     { field: "p_description", headerName: "Product Description", sortable: true, headerClass: "header-cell" },
-    { field: "", headerName: "Actions", headerClass: "header-cell", width: 250, cellRenderer: this.actionRender }
+    { field: "", headerName: "Actions", headerClass: "header-cell", width: 250, cellRenderer: this.actionRender.bind(this) }
   ];
 
   productList: any = [];
   productListSubscribe: any;
 
-  constructor(private crudService: CRUDService) {
+  constructor(private crudService: CRUDService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -47,6 +48,19 @@ export class ProductListComponent {
     '<button type="button" class="btn btn-warning">Edit</button>'
 
     div.innerHTML = htmlCode;
+
+    let viewButton = div.querySelector('.btn-success')
+    //@ts-ignore
+    viewButton?.addEventListener('click', () => {
+      this.viewProductDetails(params)
+    })
+
+    
     return div;
+  }
+
+  viewProductDetails(params: any) {
+    console.log(params)
+    this.router.navigate(['/crud/view-product-details/' + params.data.p_id])
   }
 }
