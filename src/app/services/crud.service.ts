577fc59c8fbf6edf1d3a375e5e7da80bcf919382
luvvas <luvvasm@gmail.com>
2024-02-21 14:52:31 -0300

    feat: add createproduct post service

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { HttpClient } from '@angular/common/http'
-import { map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpResponse } from "../crud/models/http-response";
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class CRUDService {
   loadProducts() {
     const url = environment.API_EndPoint + 'view.php';
     return this.httpClient.get(url).pipe(map(data => data))
+  }
+
+  createProduct(data: any): Observable<HttpResponse> {
+    const url = environment.API_EndPoint + 'create.php';
+    return this.httpClient.post<HttpResponse>(url, data).pipe(map(data => data))
   }
 }
