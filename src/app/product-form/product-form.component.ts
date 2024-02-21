39e673d714fa5bd5d56146345ce13fc02866ba7e
luvvas <luvvasm@gmail.com>
2024-02-21 14:23:32 -0300

    feat: finished product form

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CRUDService } from '../services/crud.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
@@ -7,7 +8,21 @@ import { CRUDService } from '../services/crud.service';
   styleUrl: './product-form.component.css'
 })
 export class ProductFormComponent {
-  constructor(private crudService: CRUDService) {
+  // @ts-ignore
+  productForm: FormGroup;
 
+
+  constructor(private crudService: CRUDService, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createProductForm()
+  }
+
+  createProductForm() {
+    this.productForm = this.formBuilder.group({
+      'name': ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(50)])],
+      'description': ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(500)])],
+      'price': ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(8)])]
+    })
   }
 }
